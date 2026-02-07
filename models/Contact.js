b4774639bb3d878845ca5d6bf814e2fea00d4be1
tyagi26072004 <hrithiktyagi56@gmail.com>
2026-02-07 11:05:33 +0530

    Make phone field optional in Contact model

--- models/Contact.js
+++ models/Contact.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
  const contactSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    service: String,
-    message: String,
-    phone: String
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    service: { type: String, required: true },
+    message: { type: String, required: true },
+    phone: { type: String, required: false }
  });
 
  module.exports = mongoose.model("Contact", contactSchema);
